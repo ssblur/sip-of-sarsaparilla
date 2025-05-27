5fef4224b0a443167c3c23887cb7cd22219d6d54
Patrick Hallbick <patrick.hallbick@outlook.com>
2025-05-27 17:10:37 -0400

    Add Sassafras

---
+++ common/src/main/kotlin/com/ssblur/sipofsarsaparilla/block/SassafrasSapling.kt
@@ -0,0 +1,113 @@
+package com.ssblur.sipofsarsaparilla.block
+
+import com.ssblur.unfocused.extension.BlockExtension.renderType
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.core.BlockPos
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.util.RandomSource
+import net.minecraft.world.level.BlockGetter
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.LevelReader
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.BonemealableBlock
+import net.minecraft.world.level.block.LeavesBlock
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.StateDefinition
+import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.state.properties.IntegerProperty
+import net.minecraft.world.phys.shapes.CollisionContext
+import net.minecraft.world.phys.shapes.VoxelShape
+
+class SassafrasSapling(properties: Properties) : Block(properties.randomTicks()), BonemealableBlock {
+  companion object {
+    val STAGE: IntegerProperty = BlockStateProperties.STAGE
+    val SHAPE = box(2.0, 0.0, 2.0, 14.0, 12.0, 14.0)
+  }
+
+  init {
+    registerDefaultState(defaultBlockState().setValue(STAGE, 0))
+    try { clientInit() } catch (_: NoSuchMethodError) {}
+  }
+
+  fun clientInit() {
+    this.renderType(RenderType.cutout())
+  }
+
+  override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
+    builder.add(STAGE)
+  }
+
+  override fun getShape(
+    blockState: BlockState,
+    blockGetter: BlockGetter,
+    blockPos: BlockPos,
+    collisionContext: CollisionContext
+  ): VoxelShape = SHAPE
+
+  override fun randomTick(
+    blockState: BlockState,
+    serverLevel: ServerLevel,
+    blockPos: BlockPos,
+    randomSource: RandomSource
+  ) {
+    super.randomTick(blockState, serverLevel, blockPos, randomSource)
+    // Slightly more likely than standard saplings because this tree is small (:
+    if (serverLevel.getMaxLocalRawBrightness(blockPos.above()) >= 9 && randomSource.nextInt(5) == 0) {
+      this.grow(serverLevel, blockPos, blockState)
+    }
+  }
+
+  override fun isValidBonemealTarget(levelReader: LevelReader, blockPos: BlockPos, blockState: BlockState) = true
+
+  override fun isBonemealSuccess(
+    level: Level,
+    randomSource: RandomSource,
+    blockPos: BlockPos,
+    blockState: BlockState
+  ) = level.random.nextFloat() < 0.45
+
+  override fun performBonemeal(
+    serverLevel: ServerLevel,
+    randomSource: RandomSource,
+    blockPos: BlockPos,
+    blockState: BlockState
+  ) {
+    grow(serverLevel, blockPos, blockState)
+  }
+
+  fun grow(level: Level, blockPos: BlockPos, blockState: BlockState) {
+    val max = STAGE.possibleValues.max()
+    val stage = blockState.getValue(STAGE)
+    if(stage >= max) {
+      growTree(level, blockPos)
+    } else {
+      level.setBlockAndUpdate(blockPos, blockState.setValue(STAGE, stage + 1))
+    }
+  }
+
+  fun growTree(level: Level, blockPos: BlockPos) {
+    val height = level.random.nextInt(2) + 2
+    val log = SipBlocks.SASSAFRAS_LOG.first.get().defaultBlockState()
+    for(i in 0..height) {
+      if(level.getBlockState(blockPos.above(i)).canBeReplaced() || i == 0)
+        level.setBlockAndUpdate(blockPos.above(i), log)
+    }
+
+    val leaves = SipBlocks.SASSAFRAS_LEAVES.first.get().defaultBlockState().setValue(LeavesBlock.DISTANCE, 1)
+    var pos = blockPos.above(height + 1)
+    if(level.getBlockState(pos).canBeReplaced())
+      level.setBlockAndUpdate(pos, leaves)
+    pos = blockPos.above(height).north()
+    if(level.getBlockState(pos).canBeReplaced())
+      level.setBlockAndUpdate(pos, leaves)
+    pos = blockPos.above(height).south()
+    if(level.getBlockState(pos).canBeReplaced())
+      level.setBlockAndUpdate(pos, leaves)
+    pos = blockPos.above(height).east()
+    if(level.getBlockState(pos).canBeReplaced())
+      level.setBlockAndUpdate(pos, leaves)
+    pos = blockPos.above(height).west()
+    if(level.getBlockState(pos).canBeReplaced())
+      level.setBlockAndUpdate(pos, leaves)
+  }
+}
--- common/src/main/kotlin/com/ssblur/sipofsarsaparilla/block/SipBlocks.kt
+++ common/src/main/kotlin/com/ssblur/sipofsarsaparilla/block/SipBlocks.kt
@@ -3,22 +3,56 @@ package com.ssblur.sipofsarsaparilla.block
 import com.ssblur.sipofsarsaparilla.SipOfSarsaparilla.registerBlockWithItem
 import com.ssblur.sipofsarsaparilla.item.SipItems
 import com.ssblur.unfocused.tab.CreativeTabs.tab
+import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.block.LeavesBlock
+import net.minecraft.world.level.block.RotatedPillarBlock
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties.ofFullCopy
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.material.MapColor
+import net.minecraft.world.phys.shapes.CollisionContext
+import net.minecraft.world.phys.shapes.VoxelShape
 
 object SipBlocks {
-  val OAK_SALOON_DOOR = registerBlockWithItem("oak_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.ACACIA_DOOR)) }
-  val SPRUCE_SALOON_DOOR = registerBlockWithItem("spruce_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.ACACIA_DOOR)) }
-  val BIRCH_SALOON_DOOR = registerBlockWithItem("birch_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.ACACIA_DOOR)) }
-  val JUNGLE_SALOON_DOOR = registerBlockWithItem("jungle_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.ACACIA_DOOR)) }
+  val OAK_SALOON_DOOR = registerBlockWithItem("oak_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.OAK_DOOR)) }
+  val SPRUCE_SALOON_DOOR = registerBlockWithItem("spruce_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.SPRUCE_DOOR)) }
+  val BIRCH_SALOON_DOOR = registerBlockWithItem("birch_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.BIRCH_DOOR)) }
+  val JUNGLE_SALOON_DOOR = registerBlockWithItem("jungle_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.JUNGLE_DOOR)) }
   val ACACIA_SALOON_DOOR = registerBlockWithItem("acacia_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.ACACIA_DOOR)) }
-  val DARK_OAK_SALOON_DOOR = registerBlockWithItem("dark_oak_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.ACACIA_DOOR)) }
-  val MANGROVE_SALOON_DOOR = registerBlockWithItem("mangrove_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.ACACIA_DOOR)) }
-  val CHERRY_SALOON_DOOR = registerBlockWithItem("cherry_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.ACACIA_DOOR)) }
-  val CRIMSON_SALOON_DOOR = registerBlockWithItem("crimson_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.ACACIA_DOOR)) }
-  val WARPED_SALOON_DOOR = registerBlockWithItem("warped_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.ACACIA_DOOR)) }
-  val BAMBOO_SALOON_DOOR = registerBlockWithItem("bamboo_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.ACACIA_DOOR)) }
-  val PALE_SALOON_DOOR = registerBlockWithItem("pale_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.ACACIA_DOOR)) }
+  val DARK_OAK_SALOON_DOOR = registerBlockWithItem("dark_oak_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.DARK_OAK_DOOR)) }
+  val MANGROVE_SALOON_DOOR = registerBlockWithItem("mangrove_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.MANGROVE_DOOR)) }
+  val CHERRY_SALOON_DOOR = registerBlockWithItem("cherry_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.CHERRY_DOOR)) }
+  val CRIMSON_SALOON_DOOR = registerBlockWithItem("crimson_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.CRIMSON_DOOR)) }
+  val WARPED_SALOON_DOOR = registerBlockWithItem("warped_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.WARPED_DOOR)) }
+  val BAMBOO_SALOON_DOOR = registerBlockWithItem("bamboo_saloon_door"){ SaloonDoor(ofFullCopy(Blocks.BAMBOO_DOOR)) }
+  val PALE_SALOON_DOOR = registerBlockWithItem("pale_saloon_door"){
+    SaloonDoor(ofFullCopy(Blocks.DARK_OAK_DOOR).mapColor(MapColor.COLOR_GRAY))
+  }
+
+  val SASSAFRAS_SAPLING = registerBlockWithItem("sassafras_sapling") {
+    SassafrasSapling(ofFullCopy(Blocks.SPRUCE_SAPLING))
+  }
+  val SASSAFRAS_LOG = registerBlockWithItem("sassafras_log") {
+    object: RotatedPillarBlock(ofFullCopy(Blocks.SPRUCE_LOG)) {
+      override fun getShape(
+        blockState: BlockState,
+        blockGetter: BlockGetter,
+        blockPos: BlockPos,
+        collisionContext: CollisionContext
+      ): VoxelShape {
+        if(blockState.getValue(AXIS) == Direction.Axis.Z)
+          return box(6.0, 6.0, 0.0, 10.0, 10.0, 16.0)
+        if(blockState.getValue(AXIS) == Direction.Axis.X)
+          return box(0.0, 6.0, 6.0, 16.0, 10.0, 10.0)
+        return box(6.0, 0.0, 6.0, 10.0, 16.0, 10.0)
+      }
+    }
+  }
+  val SASSAFRAS_LEAVES = registerBlockWithItem("sassafras_leaves") {
+    LeavesBlock(ofFullCopy(Blocks.SPRUCE_LEAVES))
+  }
 
   fun register() {
     OAK_SALOON_DOOR.second.tab(SipItems.TAB)
@@ -33,6 +67,10 @@ object SipBlocks {
     WARPED_SALOON_DOOR.second.tab(SipItems.TAB)
     BAMBOO_SALOON_DOOR.second.tab(SipItems.TAB)
     PALE_SALOON_DOOR.second.tab(SipItems.TAB)
+
+    SASSAFRAS_SAPLING.second.tab(SipItems.TAB)
+    SASSAFRAS_LOG.second.tab(SipItems.TAB)
+    SASSAFRAS_LEAVES.second.tab(SipItems.TAB)
   }
 
   fun registerClient() {
